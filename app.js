7ed4d6b1cabd608ffd9fffdf725e5c391d9b38aa
dhatrishdixit <dhatrish29@gmail.com>
2023-07-04 03:18:44 +0530

    third commit

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ import userRouter from "./routes/users.js";
 
 const app = express();
 
-app.use("/users",userRouter);
+//always keep router below important middleware
+// kyunki is case mein routers pehle the toh uski wajah se json ko read karne ka middle ware wala code bad mein execute hua express ki wajah se js ki wajah se nhi , jiski wajah se req.body destructure nhi ho pa raha tha 
 app.use(express.json());
+app.use("/users",userRouter);
+
 mongoose.connect("mongodb://127.0.0.1:27017",{
        dbName:"NodeAPI"
 }).then(()=>{console.log("db is connected");}).catch(e=> console.log(e))
--- routes/users.js
+++ routes/users.js
@@ -1,64 +1,20 @@
 import express from "express";
-import { User } from "../models/users.js";
 
+import {registerUser,reqparams,reqquery,specialStatic } from "../controllers/users.js";
 
 
 const router = express.Router();
 
 
-router.post("/new",async (req,res)=>{
-
-    const {name,email,password} = req.body;
-     await  User.create({
-       name,
-       email,
-       password
-    });
-    
-    res.status(201).cookie("token","check",{
-        httpOnly:true,
-        // write expires
-    }).json({
-        success:true,
-        message:"registered successfully"
-    });
-   
-})
+router.post("/new",registerUser);
 
 // using queries 
-router.get("/all",async (req,res)=>{
-    console.log(req.query);
-    console.log(typeof req);
-    // req and response are provided with object arguments , matlab woh actuaclly mein object hote hai 
-    const {name ,email} = req.query;
-    const users = await User.find({
-        // name:"dhatrish" keeping it blank will give us all the elements present in db
-        name,email
-    });
-    
+router.get("/all",reqquery);
 
-    res.json({
-        success:true,
-        users:users
-    });
-
-})
-
-router.get("/name/special",(req,res)=>{
-    res.json({
-        success:true,
-        message:"lol no one is special here everyone is treated equally "
-    })
-})
+router.get("/name/special",specialStatic);
 
 //using params , dynamic route should be kept at last or atleast lower than static routes 
-router.get("/name/:uName",async (req,res)=>{
-       const user = await User.find({name : req.params.uName});
-       res.status(201).json({
-            success:true,
-            userInfo:user
-       })
-})
+router.get("/name/:uName",reqparams)
 
 
 export default router ;
