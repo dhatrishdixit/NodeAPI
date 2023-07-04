5dd7e5b61271a5fde343fc3b8062150fcea22460
dhatrishdixit <dhatrish29@gmail.com>
2023-07-04 12:47:23 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
+import { connectDB } from "./data/database.js";
 import userRouter from "./routes/users.js";
+import { config } from "dotenv";
+config({
+    path:"./data/config.env"
+});
+export const app = express();
 
-const app = express();
 
+
+
+//middleware
 //always keep router below important middleware
 // kyunki is case mein routers pehle the toh uski wajah se json ko read karne ka middle ware wala code bad mein execute hua express ki wajah se js ki wajah se nhi , jiski wajah se req.body destructure nhi ho pa raha tha 
 app.use(express.json());
 app.use("/users",userRouter);
-
-mongoose.connect("mongodb://127.0.0.1:27017",{
-       dbName:"NodeAPI"
-}).then(()=>{console.log("db is connected");}).catch(e=> console.log(e))
 
 
 app.get("/",(req,res)=>{
@@ -19,7 +22,3 @@ app.get("/",(req,res)=>{
 })
 
 
-
-app.listen(4000,()=>{
-     console.log("server  is working");
-})
